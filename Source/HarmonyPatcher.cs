d956f67f0c399a750a31b28b59c75af4c6ca1a58
William Christian <50201676+verillious@users.noreply.github.com>
2024-05-23 19:02:15 +0100

    Storyteller settings UI (#1)
    * update settings ui lists

---
+++ Source/HarmonyPatcher.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace RandomStartMod
+{
+    [StaticConstructorOnStartup]
+    public static class RandomStart
+    {
+        static RandomStart()
+        {
+            var harmony = new Harmony("com.bogknight.RandomStart");
+            harmony.PatchAll();
+            Log.Message($"[{"RandomStartMod.Title".Translate()}] 1.0.0");
+        }
+    }
+}
---
+++ Source/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace RandomStartMod
+{
+    [HarmonyPatch(typeof(OptionListingUtility), "DrawOptionListing")]
+    public class OptionListingUtility_Patch
+    {
+        private static bool justEnteredMainMenu = false;
+
+        [HarmonyPrefix]
+        public static void Prefix(ref List<ListableOption> optList)
+        {
+            if (Current.ProgramState != ProgramState.Entry)
+            {
+                justEnteredMainMenu = false;
+                return;
+            }
+
+            if (!justEnteredMainMenu)
+            {
+                justEnteredMainMenu = true;
+            }
+
+            // Iterate over each option in the optList collection
+            foreach (ListableOption opt in optList)
+            {
+                // Check if the option has a non-null action, indicating that are the main menu buttons
+                if (opt.action != null)
+                {
+                    ListableOption newOption = new ListableOption(
+                    "Random".Translate(),
+                    delegate {
+                        LongEventHandler.QueueLongEvent(delegate
+                            {
+                                RandomScenario.SetupForRandomPlay();
+                            }, "GeneratingMap", doAsynchronously: false, GameAndMapInitExceptionHandlers.ErrorWhileGeneratingMap);
+                    },
+                    null
+                    );
+                    optList.Insert(0, newOption);
+                    break;
+                }
+            }
+        }
+    }
+}
